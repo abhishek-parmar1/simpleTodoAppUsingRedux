92247d79025ef17989051ce73f1d86a5bcc09dba
Abhishek Parmar <parmarabhishek.53@gmail.com>
2018-07-30 08:55:13 +0530

    structure fixes
    changes in code structure

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 // presentational components
-const todo = ({ onClick, id, text, completed }) => {
-  return (
-    <li
-      onClick={onClick}
-      style={{
-        textDecoration: completed ? "line-through" : "none"
-      }}
-    >
-      {id}||{text}
-    </li>
-  );
-};
-
+const todo = ({ onClick, id, text, completed }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? "line-through" : "none"
+    }}
+  >
+    {id}||{text}
+  </li>
+);
 export default todo;
--- src/components/todoList/todoList.js
+++ src/components/todoList/todoList.js
@@ -3,14 +3,11 @@ import React from "react";
 // import components here
 import Todo from "../todo/todo";
 
-const TodoList = ({ todos, onTodoClick }) => {
-  return (
-    <ul>
-      {todos.map(todo => (
-        <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
-      ))}
-    </ul>
-  );
-};
-
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    ))}
+  </ul>
+);
 export default TodoList;
